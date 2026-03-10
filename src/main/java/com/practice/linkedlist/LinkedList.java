0aecca071d609d287bb93746f1e2255996f4f6d7
Sunil Sharma <sunil.sharma6@outlook.com>
2026-03-11 01:13:13 +0530

    Added getLength and getMiddle(brute)

--- src/main/java/com/practice/linkedlist/LinkedList.java
+++ src/main/java/com/practice/linkedlist/LinkedList.java
@@ -20,6 +20,33 @@ public class LinkedList {
         current.next = new Node(data);
      }
 
+     public int getLength(){
+         Node current = head;
+         if(current == null){
+             System.out.print("Empty List");
+             return -1;
+         }
+         int length = 0;
+         while(current.next != null){
+
+             current = current.next;
+             length++;
+         }
+         return length + 1;
+     }
+
+     public int getMiddle(){
+         Node current = head;
+         int length = getLength();
+
+         int mid = length/2;
+         while (mid > 0){
+             current = current.next;
+             mid--;
+         }
+         return current.data;
+     }
+
      public void printList(){
          Node current = head;
          if(current == null){
@@ -27,18 +54,22 @@ public class LinkedList {
              return;
          }
          while(current.next != null){
-             System.out.println(current.data);
+             System.out.print(current.data + "-> ");
              current = current.next;
          }
-         System.out.println(current.data);
+         System.out.print(current.data + "-> ");
      }
-
-    public static void main(String[] args) {
+     static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.append(1);
         ll.append(2);
-        ll.append(4);
+        ll.append(3);
+        ll.append(99);
+        ll.append(5);
+        ll.append(6);
         ll.printList();
+        System.out.println("Length = " + ll.getLength());
+        System.out.println("Middle = " + ll.getMiddle());
     }
 
 }
